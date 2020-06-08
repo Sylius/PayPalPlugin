6aef95a30032a3255241f67d076c7a4918445a81
SirDomin <dominik.garbulski@sylius.com>
2020-06-08 12:06:06 +0200

    PayPal method is always as first choice

---
+++ src/ChannelBasedPaymentMethodsResolver.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\PayPalPlugin;
+
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Core\Repository\PaymentMethodRepositoryInterface;
+use Sylius\Component\Payment\Model\PaymentInterface as BasePaymentInterface;
+use Sylius\Component\Payment\Resolver\PaymentMethodsResolverInterface;
+use Webmozart\Assert\Assert;
+
+final class ChannelBasedPaymentMethodsResolver implements PaymentMethodsResolverInterface
+{
+    /** @var PaymentMethodRepositoryInterface */
+    private $paymentMethodRepository;
+
+    public function __construct(PaymentMethodRepositoryInterface $paymentMethodRepository)
+    {
+        $this->paymentMethodRepository = $paymentMethodRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSupportedMethods(BasePaymentInterface $payment): array
+    {
+        /** @var PaymentInterface $payment */
+        Assert::isInstanceOf($payment, PaymentInterface::class);
+        Assert::true($this->supports($payment), 'This payment method is not support by resolver');
+        return $this->sortPayments(
+            $this->paymentMethodRepository->findEnabledForChannel($payment->getOrder()->getChannel()),
+            'sylius.pay_pal'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(BasePaymentInterface $payment): bool
+    {
+        return $payment instanceof PaymentInterface &&
+            null !== $payment->getOrder() &&
+            null !== $payment->getOrder()->getChannel()
+            ;
+    }
+
+    private function sortPayments(array $payments, string $first_payment): array
+    {
+        $sorted_payments = [];
+
+        foreach($payments as $payment){
+            if($payment->getGatewayConfig()->getFactoryName() === $first_payment)
+                array_unshift($sorted_payments, $payment);
+            else
+                $sorted_payments[] = $payment;
+        }
+        return $sorted_payments;
+    }
+}
---
+++ tests/Behat/Context/Setup/PaymentPayPalContext.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\PayPalPlugin\Behat\Context\Setup;
+
+use Behat\Behat\Context\Context;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sylius\Behat\Service\SharedStorageInterface;
+use Sylius\Bundle\CoreBundle\Fixture\Factory\ExampleFactoryInterface;
+use Sylius\Component\Core\Model\PaymentMethodInterface;
+use Sylius\Component\Payment\Repository\PaymentMethodRepositoryInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+
+final class PaymentPayPalContext implements Context
+{
+    /** @var SharedStorageInterface */
+    private $sharedStorage;
+
+    /** @var PaymentMethodRepositoryInterface */
+    private $paymentMethodRepository;
+
+    /** @var ExampleFactoryInterface */
+    private $paymentMethodExampleFactory;
+
+    /** @var FactoryInterface */
+    private $paymentMethodTranslationFactory;
+
+    /** @var ObjectManager */
+    private $paymentMethodManager;
+
+    /** @var array */
+    private $gatewayFactories;
+
+    public function __construct(
+        SharedStorageInterface $sharedStorage,
+        PaymentMethodRepositoryInterface $paymentMethodRepository,
+        ExampleFactoryInterface $paymentMethodExampleFactory,
+        FactoryInterface $paymentMethodTranslationFactory,
+        ObjectManager $paymentMethodManager,
+        array $gatewayFactories
+    ) {
+        $this->sharedStorage = $sharedStorage;
+        $this->paymentMethodRepository = $paymentMethodRepository;
+        $this->paymentMethodExampleFactory = $paymentMethodExampleFactory;
+        $this->paymentMethodTranslationFactory = $paymentMethodTranslationFactory;
+        $this->paymentMethodManager = $paymentMethodManager;
+        $this->gatewayFactories = $gatewayFactories;
+    }
+
+    /**
+     * @param string $name
+     * @param string $code
+     * @param string $gatewayFactory
+     * @param string $description
+     * @param bool $addForCurrentChannel
+     * @param int|null $position
+     *
+     * @return PaymentMethodInterface
+     */
+    private function createPaymentMethod(
+        $name,
+        $code,
+        $gatewayFactory = 'Offline',
+        $description = '',
+        $addForCurrentChannel = true,
+        $position = null
+    ) {
+        $gatewayFactory = array_search($gatewayFactory, $this->gatewayFactories);
+
+        /** @var PaymentMethodInterface $paymentMethod */
+        $paymentMethod = $this->paymentMethodExampleFactory->create([
+            'name' => ucfirst($name),
+            'code' => $code,
+            'description' => $description,
+            'gatewayName' => $gatewayFactory,
+            'gatewayFactory' => $gatewayFactory,
+            'enabled' => true,
+            'channels' => ($addForCurrentChannel && $this->sharedStorage->has('channel')) ? [$this->sharedStorage->get('channel')] : [],
+        ]);
+
+        if (null !== $position) {
+            $paymentMethod->setPosition((int) $position);
+        }
+
+        $this->sharedStorage->set('payment_method', $paymentMethod);
+        $this->paymentMethodRepository->add($paymentMethod);
+
+        return $paymentMethod;
+    }
+
+    /**
+     * @Given /^the store allows paying with "([^"]*)" with "([^"]*)" factory name at position (\d+)$/
+     */
+    public function theStoreAllowsPayingWithWithFactoryNameAtPosition($paymentMethodName, $gatewayFactory = null, $position = null)
+    {
+        $this->createPaymentMethod($paymentMethodName, 'PM_' . $paymentMethodName, $gatewayFactory, 'Payment method', true, $position);
+    }
+}
