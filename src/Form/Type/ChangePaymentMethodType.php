5e85bfc1a3540fa6159f00445de0d52fca651a9e
Mateusz Zalewski <zaleslaw@gmail.com>
2020-08-11 11:07:32 +0200

    Override checkout payment step to handle processing payments

---
+++ src/Form/Type/ChangePaymentMethodType.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\PayPalPlugin\Form\Type;
+
+use Sylius\Component\Core\Model\PaymentInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+final class ChangePaymentMethodType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event): void {
+            $payments = $event->getData();
+            $form = $event->getForm();
+
+            foreach ($payments as $key => $payment) {
+                if (!in_array(
+                    $payment->getState(),
+                    [PaymentInterface::STATE_NEW, PaymentInterface::STATE_CART, PaymentInterface::STATE_PROCESSING]
+                )) {
+                    $form->remove($key);
+                }
+            }
+        });
+    }
+
+    public function getParent(): string
+    {
+        return CollectionType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'sylius_change_payment_method';
+    }
+}
