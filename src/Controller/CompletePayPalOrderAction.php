28a0b089529052535a8a50980470297072bc1440
Kamil Kokot <kamil@kokot.me>
2020-08-28 13:19:50 +0100

    Remove unneeded dependencies from CompletePayPalOrderAction

--- src/Controller/CompletePayPalOrderAction.php
+++ src/Controller/CompletePayPalOrderAction.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Sylius\PayPalPlugin\Controller;
 
-use Doctrine\Persistence\ObjectManager;
-use Payum\Core\Payum;
-use SM\Factory\FactoryInterface;
 use Sylius\Component\Core\Model\PaymentInterface;
-use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Sylius\PayPalPlugin\Manager\PaymentStateManagerInterface;
 use Sylius\PayPalPlugin\Provider\OrderProviderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,20 +14,8 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class CompletePayPalOrderAction
 {
-    /** @var Payum */
-    private $payum;
-
-    /** @var OrderRepositoryInterface */
-    private $orderRepository;
-
-    /** @var FactoryInterface */
-    private $stateMachineFactory;
-
     /** @var PaymentStateManagerInterface */
     private $paymentStateManager;
-
-    /** @var ObjectManager */
-    private $paymentManager;
 
     /** @var UrlGeneratorInterface */
     private $router;
@@ -40,19 +24,11 @@ final class CompletePayPalOrderAction
     private $orderProvider;
 
     public function __construct(
-        Payum $payum,
-        OrderRepositoryInterface $orderRepository,
-        FactoryInterface $stateMachineFactory,
         PaymentStateManagerInterface $paymentStateManager,
-        ObjectManager $paymentManager,
         UrlGeneratorInterface $router,
         OrderProviderInterface $orderProvider
     ) {
-        $this->payum = $payum;
-        $this->orderRepository = $orderRepository;
-        $this->stateMachineFactory = $stateMachineFactory;
         $this->paymentStateManager = $paymentStateManager;
-        $this->paymentManager = $paymentManager;
         $this->router = $router;
         $this->orderProvider = $orderProvider;
     }
