383bd91a9268a4e8f5ee76278ff7b520af5c7131
SirDomin <dominik.garbulski@sylius.com>
2020-10-26 14:23:38 +0100

    log payments error when onError triggers

--- src/Controller/PayPalPaymentOnErrorAction.php
+++ src/Controller/PayPalPaymentOnErrorAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sylius\PayPalPlugin\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
@@ -13,14 +14,18 @@ final class PayPalPaymentOnErrorAction
     /** @var FlashBagInterface */
     private $flashBag;
 
-    public function __construct(FlashBagInterface $flashBag)
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(FlashBagInterface $flashBag, LoggerInterface $logger)
     {
         $this->flashBag = $flashBag;
+        $this->logger = $logger;
     }
 
     public function __invoke(Request $request): Response
     {
-        // TODO log error somewhere
+        $this->logger->error((string) $request->getContent());
         $this->flashBag->add('error', 'sylius.pay_pal.something_went_wrong');
 
         return new Response();
