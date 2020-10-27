4cf81510329fbbda4d23e8e25a923d30a7946b1e
SirDomin <dominik.garbulski@sylius.com>
2020-10-27 15:38:26 +0100

    use locale from symfony

--- spec/Processor/LocaleProcessorSpec.php
+++ spec/Processor/LocaleProcessorSpec.php
@@ -10,8 +10,9 @@ final class LocaleProcessorSpec extends ObjectBehavior
 {
     public function it_processes_locale_to_match_locale_regex(): void
     {
-        $this->process('uk')->shouldReturn('uk_UK');
+        $this->process('et')->shouldReturn('et_EE');
         $this->process('pl')->shouldReturn('pl_PL');
+        $this->process('ja')->shouldReturn('ja_JP');
     }
 
     public function it_returns_same_locale_if_already_matches_regex(): void
--- src/Processor/LocaleProcessor.php
+++ src/Processor/LocaleProcessor.php
@@ -4,14 +4,27 @@ declare(strict_types=1);
 
 namespace Sylius\PayPalPlugin\Processor;
 
+use Symfony\Component\Intl\Locales;
+
 final class LocaleProcessor implements LocaleProcessorInterface
 {
     public function process(string $locale): string
     {
-        if (preg_match('/^[a-z]{2}_[A-Z]{2}$/', $locale)) {
+        if ($this->isValidLocale($locale)) {
             return $locale;
         }
 
-        return $locale . '_' . strtoupper($locale);
+        $locales = Locales::getLocales();
+        $localeFound = (int) array_search($locale, $locales);
+        while (!$this->isValidLocale($locales[$localeFound]) && $localeFound < count($locales)) {
+            ++$localeFound;
+        }
+
+        return $locales[$localeFound];
+    }
+
+    private function isValidLocale(string $locale): bool
+    {
+        return preg_match('/^[a-z]{2}_[A-Z]{2}$/', $locale) > 0;
     }
 }
