5ee44e40414508fc86777b648823ccbc716ba67d
SirDomin <domin12wsx@gmail.com>
2021-10-27 10:51:00 +0200

    refactor state machine

--- src/Controller/CancelLastPayPalPaymentAction.php
+++ src/Controller/CancelLastPayPalPaymentAction.php
@@ -11,22 +11,13 @@ use Sylius\Component\Core\Model\PaymentInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Sylius\Component\Order\Processor\OrderProcessorInterface;
 use Sylius\Component\Payment\PaymentTransitions;
-use Sylius\PayPalPlugin\Provider\PaymentProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
-use Webmozart\Assert\Assert;
 
 final class CancelLastPayPalPaymentAction
 {
-    /** @var PaymentProviderInterface */
-    private $paymentProvider;
-
     /** @var ObjectManager */
     private $objectManager;
-
-    /** @var FlashBag */
-    private $flashBag;
 
     /** @var FactoryInterface */
     private $stateMachineFactory;
@@ -38,16 +29,12 @@ final class CancelLastPayPalPaymentAction
     private $orderRepository;
 
     public function __construct(
-        PaymentProviderInterface $paymentProvider,
         ObjectManager $objectManager,
-        FlashBag $flashBag,
         FactoryInterface $stateMachineFactory,
         OrderProcessorInterface $orderPaymentProcessor,
         OrderRepositoryInterface $orderRepository
     ) {
-        $this->paymentProvider = $paymentProvider;
         $this->objectManager = $objectManager;
-        $this->flashBag = $flashBag;
         $this->stateMachineFactory = $stateMachineFactory;
         $this->orderPaymentProcessor = $orderPaymentProcessor;
         $this->orderRepository = $orderRepository;
@@ -61,15 +48,11 @@ final class CancelLastPayPalPaymentAction
         /** @var PaymentInterface $payment */
         $payment = $order->getLastPayment();
 
-        if ($payment->getState() === PaymentInterface::STATE_CART || $payment->getState() === PaymentInterface::STATE_PROCESSING) {
-            $payment->setState(PaymentInterface::STATE_NEW);
-            $paymentStateMachine = $this->stateMachineFactory->get($payment, PaymentTransitions::GRAPH);
+        $paymentStateMachine = $this->stateMachineFactory->get($payment, PaymentTransitions::GRAPH);
+        $paymentStateMachine->apply(PaymentTransitions::TRANSITION_CANCEL);
 
-            $paymentStateMachine->apply(PaymentTransitions::TRANSITION_CANCEL);
-
-            $this->orderPaymentProcessor->process($order);
-            $this->objectManager->flush();
-        }
+        $this->orderPaymentProcessor->process($order);
+        $this->objectManager->flush();
 
         return new Response('', Response::HTTP_NO_CONTENT);
     }
