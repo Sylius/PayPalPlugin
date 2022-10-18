dfbf928e42fa30d5b9f8ce355265f37c0901fa6e
Jan Goralski <jan.wojciech.goralski@gmail.com>
2022-10-18 09:36:21 +0200

    [Maintenance] Conditional Swiftmailer/Symfony Mailer config

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -7,24 +7,12 @@ namespace Tests\Sylius\PayPalPlugin\Application;
 use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
 use Sylius\Bundle\CoreBundle\Application\Kernel as SyliusKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\DelegatingLoader;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\DependencyInjection\Loader\ClosureLoader;
-use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
-use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
-use Symfony\Component\DependencyInjection\Loader\IniFileLoader;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\Config\FileLocator;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
-use Webmozart\Assert\Assert;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
@@ -45,13 +33,6 @@ final class Kernel extends BaseKernel
     {
         $contents = require $this->getProjectDir() . '/config/bundles.php';
 
-        if (SyliusKernel::MINOR_VERSION > 10) {
-            $contents = array_merge(
-                ['Sylius\Calendar\SyliusCalendarBundle' => ['all' => true]],
-                $contents
-            );
-        }
-
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
                 yield new $class();
@@ -63,21 +44,22 @@ final class Kernel extends BaseKernel
     {
         $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
         $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir() . '/config';
 
-        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        foreach ($this->getConfigDirectories() as $confDir) {
+            $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+            $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+            $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+            $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        }
     }
 
-    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $confDir = $this->getProjectDir() . '/config';
-
-        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+        foreach ($this->getConfigDirectories() as $confDir) {
+            $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS);
+            $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS);
+            $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS);
+        }
     }
 
     protected function getContainerBaseClass(): string
@@ -87,6 +69,14 @@ final class Kernel extends BaseKernel
         }
 
         return parent::getContainerBaseClass();
+    }
+
+    private function getConfigDirectories(): array
+    {
+        return array_filter([
+            $this->getProjectDir() . '/config',
+            $this->getProjectDir() . '/config/sylius/' . SyliusKernel::MAJOR_VERSION . '.' . SyliusKernel::MINOR_VERSION,
+        ], 'file_exists');
     }
 
     private function isTestEnvironment(): bool
--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -6,7 +6,6 @@ $bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sylius\Bundle\OrderBundle\SyliusOrderBundle::class => ['all' => true],
@@ -60,9 +59,12 @@ $bundles = [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
 ];
 
-if (Kernel::MINOR_VERSION >= 12) {
+if (Kernel::MINOR_VERSION < 12) {
+    $bundles[Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class] = ['all' => true];
+} else {
     $bundles[League\FlysystemBundle\FlysystemBundle::class] = ['all' => true];
 }
 
