23c951e3fa8dc47a4dee836b748859ee2dabbd04
Rafikooo <thecrazyrafik@gmail.com>
2023-04-11 16:20:18 +0200

    [ECS] Update config file

--- ecs.php
+++ ecs.php
@@ -3,12 +3,13 @@
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use SlevomatCodingStandard\Sniffs\Commenting\InlineDocCommentDeclarationSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+return static function (ECSConfig $config): void {
+    $config->import('vendor/sylius-labs/coding-standard/ecs.php');
 
-    $containerConfigurator->parameters()->set(Option::SKIP, [
+    $config->skip([
         VisibilityRequiredFixer::class => ['*Spec.php'],
         InlineDocCommentDeclarationSniff::class . '.MissingVariable',
     ]);
