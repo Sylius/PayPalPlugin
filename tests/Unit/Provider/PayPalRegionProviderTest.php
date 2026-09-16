71d8ff60784224e8e06a6b91fb6727f34a03d519
Grzegorz Sadowski <sadowskigp@gmail.com>
2026-09-16 07:22:12 +0200

    Fold the region cases into a data provider and assert the wallet addresses

--- tests/Unit/Provider/PayPalRegionProviderTest.php
+++ tests/Unit/Provider/PayPalRegionProviderTest.php
@@ -13,75 +13,37 @@ declare(strict_types=1);
 
 namespace Tests\Sylius\PayPalPlugin\Unit\Provider;
 
-use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Sylius\Component\Core\Model\AddressInterface;
 use Sylius\PayPalPlugin\Provider\PayPalRegionProvider;
 
 final class PayPalRegionProviderTest extends TestCase
 {
-    private AddressInterface&MockObject $address;
+    #[DataProvider('regionProvider')]
+    public function test_it_provides_the_region_pay_pal_expects_for_an_address(
+        ?string $countryCode,
+        ?string $provinceCode,
+        ?string $provinceName,
+        ?string $expectedRegion,
+    ): void {
+        $address = $this->createMock(AddressInterface::class);
+        $address->method('getCountryCode')->willReturn($countryCode);
+        $address->method('getProvinceCode')->willReturn($provinceCode);
+        $address->method('getProvinceName')->willReturn($provinceName);
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->address = $this->createMock(AddressInterface::class);
+        self::assertSame($expectedRegion, PayPalRegionProvider::provide($address));
     }
 
-    public function test_it_strips_the_country_prefix_from_the_province_code(): void
+    /** @return iterable<string, array{?string, ?string, ?string, ?string}> */
+    public static function regionProvider(): iterable
     {
-        $this->address->method('getCountryCode')->willReturn('US');
-        $this->address->method('getProvinceCode')->willReturn('US-TX');
-
-        self::assertSame('TX', PayPalRegionProvider::provide($this->address));
-    }
-
-    public function test_it_keeps_a_province_code_that_carries_no_country_prefix(): void
-    {
-        $this->address->method('getCountryCode')->willReturn('US');
-        $this->address->method('getProvinceCode')->willReturn('TX');
-
-        self::assertSame('TX', PayPalRegionProvider::provide($this->address));
-    }
-
-    public function test_it_keeps_a_province_code_prefixed_with_another_country(): void
-    {
-        $this->address->method('getCountryCode')->willReturn('US');
-        $this->address->method('getProvinceCode')->willReturn('CA-ON');
-
-        self::assertSame('CA-ON', PayPalRegionProvider::provide($this->address));
-    }
-
-    public function test_it_falls_back_to_the_province_name_when_there_is_no_province_code(): void
-    {
-        $this->address->method('getCountryCode')->willReturn('PL');
-        $this->address->method('getProvinceName')->willReturn('Mazowieckie');
-
-        self::assertSame('Mazowieckie', PayPalRegionProvider::provide($this->address));
-    }
-
-    public function test_it_prefers_the_province_code_over_the_province_name(): void
-    {
-        $this->address->method('getCountryCode')->willReturn('US');
-        $this->address->method('getProvinceCode')->willReturn('US-TX');
-        $this->address->method('getProvinceName')->willReturn('Texas');
-
-        self::assertSame('TX', PayPalRegionProvider::provide($this->address));
-    }
-
-    public function test_it_provides_nothing_when_the_address_carries_neither(): void
-    {
-        $this->address->method('getCountryCode')->willReturn('US');
-
-        self::assertNull(PayPalRegionProvider::provide($this->address));
-    }
-
-    public function test_it_treats_a_blank_region_as_absent(): void
-    {
-        $this->address->method('getCountryCode')->willReturn('US');
-        $this->address->method('getProvinceCode')->willReturn('   ');
-        $this->address->method('getProvinceName')->willReturn('');
-
-        self::assertNull(PayPalRegionProvider::provide($this->address));
+        yield 'province code carrying the country prefix' => ['US', 'US-TX', null, 'TX'];
+        yield 'province code without a country prefix' => ['US', 'TX', null, 'TX'];
+        yield 'province code prefixed with another country' => ['US', 'CA-ON', null, 'CA-ON'];
+        yield 'province name only' => ['PL', null, 'Mazowieckie', 'Mazowieckie'];
+        yield 'province code over province name' => ['US', 'US-TX', 'Texas', 'TX'];
+        yield 'neither code nor name' => ['US', null, null, null];
+        yield 'blank code and blank name' => ['US', '   ', '', null];
     }
 }
