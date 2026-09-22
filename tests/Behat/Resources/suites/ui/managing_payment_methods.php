9acc1a16a7b254c462095392be44c0061c0fd5f2
TheMilek <kam.grygierzec@gmail.com>
2026-09-22 09:39:17 +0200

    Migrate Behat configuration from YAML to PHP

---
+++ tests/Behat/Resources/suites/ui/managing_payment_methods.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Behat\Config\Config;
+use Behat\Config\Filter\TagFilter;
+use Behat\Config\Profile;
+use Behat\Config\Suite;
+use Tests\Sylius\PayPalPlugin\Behat\Context\Admin\ManagingPaymentMethodsContext;
+use Tests\Sylius\PayPalPlugin\Behat\Context\Setup\PaymentPayPalContext;
+
+return (new Config())
+    ->withProfile(
+        (new Profile('default'))
+        ->withSuite(
+            (new Suite('ui_managing_payment_methods'))
+            ->withContexts(
+                'sylius.behat.context.hook.doctrine_orm',
+            )
+            ->withContexts(
+                'sylius.behat.context.transform.address',
+                'sylius.behat.context.transform.channel',
+                'sylius.behat.context.transform.country',
+                'sylius.behat.context.transform.customer',
+                'sylius.behat.context.transform.order',
+                'sylius.behat.context.transform.payment',
+                'sylius.behat.context.transform.product',
+                'sylius.behat.context.transform.shared_storage',
+                'sylius.behat.context.transform.shipping_method',
+                'sylius.behat.context.transform.zone',
+                'sylius.behat.context.transform.lexical',
+            )
+            ->withContexts(
+                'sylius.behat.context.setup.admin_security',
+                'sylius.behat.context.setup.channel',
+                'sylius.behat.context.setup.customer',
+                'sylius.behat.context.setup.geographical',
+                'sylius.behat.context.setup.order',
+                'sylius.behat.context.setup.payment',
+                'sylius.behat.context.setup.product',
+                'sylius.behat.context.setup.shipping',
+                'sylius.behat.context.setup.zone',
+                PaymentPayPalContext::class,
+            )
+            ->withContexts(
+                'sylius.behat.context.ui.admin.managing_payment_methods',
+                'sylius.behat.context.ui.channel',
+                'sylius.behat.context.ui.email',
+                'sylius.behat.context.ui.shop.cart',
+                ManagingPaymentMethodsContext::class,
+            )
+            ->withFilter(new TagFilter('@managing_payment_methods&&@ui'))
+            ->withPaths('features'),
+        ),
+    )
+;
