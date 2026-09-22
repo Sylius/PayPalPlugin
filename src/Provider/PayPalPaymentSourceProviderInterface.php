f6a9b876d1f0f89b56c91202eb72b4b72ad2d8ec
Grzegorz Sadowski <sadowskigp@gmail.com>
2026-09-22 06:37:55 +0200

    Declare the redirect payment methods PayPal offers

--- src/Provider/PayPalPaymentSourceProviderInterface.php
+++ src/Provider/PayPalPaymentSourceProviderInterface.php
@@ -15,12 +15,24 @@ namespace Sylius\PayPalPlugin\Provider;
 
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\PayPalPlugin\Exception\UnsupportedPayPalPaymentSourceException;
+use Sylius\PayPalPlugin\Model\RedirectPaymentSource;
 
 interface PayPalPaymentSourceProviderInterface
 {
     public const PAYPAL = 'paypal';
 
     public const GOOGLE_PAY = 'google_pay';
+
+    public const TRUSTLY = RedirectPaymentSource::Trustly->value;
+
+    /** @var list<string> */
+    public const BASE_EXPERIENCE_CONTEXT_KEYS = [
+        'brand_name',
+        'locale',
+        'shipping_preference',
+        'return_url',
+        'cancel_url',
+    ];
 
     /**
      * @param array<string, mixed> $experienceContext
