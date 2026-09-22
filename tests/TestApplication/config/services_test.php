05b8681d82b4ed8684589ae145fad9c6bf61c572
TheMilek <kam.grygierzec@gmail.com>
2026-09-22 09:39:16 +0200

    Load Sylius Behat services from PHP config on Symfony 8

--- tests/TestApplication/config/services_test.php
+++ tests/TestApplication/config/services_test.php
@@ -5,8 +5,11 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return function (ContainerConfigurator $container) {
     $env = $_ENV['APP_ENV'] ?? 'dev';
 
-    if (str_starts_with($env, 'test')) {
-        $container->import('../../../vendor/sylius/sylius/src/Sylius/Behat/Resources/config/services.xml');
+    if (str_starts_with((string) $env, 'test')) {
+        // Sylius 2.3 ships its Behat services as a PHP config, earlier versions only as XML,
+        // which Symfony 8 can no longer load.
+        $syliusBehatServices = '../../../vendor/sylius/sylius/src/Sylius/Behat/Resources/config/services';
+        $container->import(is_file(__DIR__ . '/' . $syliusBehatServices . '.php') ? $syliusBehatServices . '.php' : $syliusBehatServices . '.xml');
         $container->import('@SyliusPayPalPlugin/tests/Behat/Resources/services.php');
         $container->import('@SyliusPayPalPlugin/tests/TestApplication/config/services_test.yaml');
     }
